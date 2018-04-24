183d435de814b253f2d0a32ba9bf203e98eb358d
michellekondou <michellekondou@gmail.com>
2018-04-24 12:21:05 +0300

    apply corrections to archive and add titles

--- template-parts/post/content-front.php
+++ template-parts/post/content-front.php
@@ -27,24 +27,48 @@
             325px"
     src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"
     class="lazyload">
+    	<header class="entry-header">
+		<?php
+		if ( 'post' === get_post_type() ) {
+			//echo '<div class="entry-meta">';
+				if ( is_single() ) {
+					//pop_posted_on();
+				} else {
+					//echo pop_time_link();
+					//pop_edit_link();
+				};
+			//echo '</div><!-- .entry-meta -->';
+		};
+
+		if ( is_single() ) {
+			the_title( '<h1 class="entry-title">', '</h1>' );
+		} elseif ( is_front_page() && is_home() ) {
+			the_title( '<h3 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h3>' );
+		} else {
+			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+		}
+		?>
+        </header><!-- .entry-header -->
+        <script>
+            var cw = window.innerWidth;
+            console.log(cw);
+            var gridItem = document.querySelectorAll('#post-'+ <?php the_ID(); ?> + '.grid-item');
+            var gridItemWidth;
+            var gridItemHeight;
+            if(cw <= 360) {
+                gridItemWidth = <?php echo $image_data_xs[1]; ?>;
+                gridItemHeight = <?php echo $image_data_xs[2]+24; ?>;
+            } else if (cw > 360 && cw < 465) {
+                gridItemWidth = <?php echo $image_data_s[1]; ?>;
+                gridItemHeight = <?php echo $image_data_s[2]+24; ?>;
+            } else if (cw > 465) {
+                gridItemWidth = <?php echo $image_data[1]; ?>;
+                gridItemHeight = <?php echo $image_data[2]+24; ?>;
+            }
+            
+            gridItem[0].style.width = gridItemWidth +'px';
+            gridItem[0].style.height = gridItemHeight +'px';
+        </script>
 </article>
-<script>
-    var cw = window.innerWidth;
-    console.log(cw);
-    var gridItem = document.querySelectorAll('#post-'+ <?php the_ID(); ?> + '.grid-item');
-    var gridItemWidth;
-    var gridItemHeight;
-    if(cw <= 360) {
-        gridItemWidth = <?php echo $image_data_xs[1]; ?>;
-        gridItemHeight = <?php echo $image_data_xs[2]+24; ?>;
-    } else if (cw > 360 && cw < 465) {
-        gridItemWidth = <?php echo $image_data_s[1]; ?>;
-        gridItemHeight = <?php echo $image_data_s[2]+24; ?>;
-    } else if (cw > 465) {
-        gridItemWidth = <?php echo $image_data[1]; ?>;
-        gridItemHeight = <?php echo $image_data[2]+24; ?>;
-    }
-    
-    gridItem[0].style.width = gridItemWidth +'px';
-    gridItem[0].style.height = gridItemHeight +'px';
-</script>
+
+
